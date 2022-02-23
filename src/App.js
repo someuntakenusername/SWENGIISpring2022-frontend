5671ccf7a424b73542c38e037b92620f949f5f9f
SamBeyerMM <75656515+SamBeyerMM@users.noreply.github.com>
2022-02-23 12:11:23 -0600

    Google Maps Integration

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import UserComponent from './components/UserComponent';
+import  {MapComponent}  from './components/MapComponent';
 import './App.css';
+import useWindowDimensions from "./FunctionReferences/WindowDimensions"
 
 function App() {
+
+  const { height, width } = useWindowDimensions();
+
   return (
     <div className="App">
-        <UserComponent />
+       <MapComponent
+       googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCJYrN6ByIeKbZxymQ7LaESn-lHUhMZEXE&v=3.exp&libraries=geometry,drawing,places"
+       loadingElement={<div style={{ height: `100%` }} />}
+       containerElement={<div style={{ height: height }} />}
+       mapElement={<div style={{ height: width }} />}
+       ></MapComponent>
     </div>
   );
 }
