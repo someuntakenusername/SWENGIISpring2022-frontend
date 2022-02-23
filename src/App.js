b92f11711fb25f54c0ef10d1ae31b313988862d1
SamBeyerMM <75656515+SamBeyerMM@users.noreply.github.com>
2022-02-23 13:26:36 -0600

    Yelp API

--- src/App.js
+++ src/App.js
@@ -2,20 +2,52 @@ import React from 'react';
 import  {MapComponent}  from './components/MapComponent';
 import './App.css';
 import useWindowDimensions from "./FunctionReferences/WindowDimensions"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import YelpComponent from './components/YelpDemoComponent';
 
 function App() {
 
   const { height, width } = useWindowDimensions();
 
   return (
-    <div className="App">
-       <MapComponent
-       googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCJYrN6ByIeKbZxymQ7LaESn-lHUhMZEXE&v=3.exp&libraries=geometry,drawing,places"
-       loadingElement={<div style={{ height: `100%` }} />}
-       containerElement={<div style={{ height: height }} />}
-       mapElement={<div style={{ height: width }} />}
-       ></MapComponent>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/home">Google Maps API</Link>
+            </li>
+            <li>
+              <Link to="/yelp">Yelp API</Link>
+            </li>
+            
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/home" element ={
+            <div className="App">
+              <MapComponent
+              googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCJYrN6ByIeKbZxymQ7LaESn-lHUhMZEXE&v=3.exp&libraries=geometry,drawing,places"
+              loadingElement={<div style={{ height: `100%` }} />}
+              containerElement={<div style={{ height: height }} />}
+              mapElement={<div style={{ height: width }} />}
+              >
+              </MapComponent>
+            </div>
+          }>
+          </Route>
+          <Route path="/yelp" element ={
+            <YelpComponent></YelpComponent>
+          }>
+          </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 export default App;
--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -3,7 +3,6 @@ import UserService from '../services/UserService';
 import {GoogleMap, withGoogleMap, withScriptjs} from "react-google-maps"
 
 
-
 export const MapComponent = withScriptjs(withGoogleMap((props) =>
   <GoogleMap
     defaultZoom={8}
